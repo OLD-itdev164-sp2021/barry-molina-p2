df42b02916475304e145f773fd3ff8a4d92ac837
Barry Molina <bazzaboy@gmail.com>
2021-05-06 18:18:15 -0500

    Add grid

--- roerick-portfolio/pages/index.js
+++ roerick-portfolio/pages/index.js
@@ -11,20 +11,19 @@ const Intro = styled(Section)`
 `
 
 const Description = styled(Section)`
-  height: 200px;
+  height: 400px;
   background-color: purple;
 `
 
 const Categories = styled(Section)`
-  margin-top: -70px;
+  margin-top: -200px;
   padding: 2rem;
 `
 
 const Columns = styled(Flex)`
   background-color: white;
-  min-height: 300px;
   /* border: 1px solid black; */
-  max-width: 850px;
+  max-width: 1200px;
   margin: 0 auto;
   border-radius: 8px;
   box-shadow: .5px .5px 4px black;
@@ -35,6 +34,7 @@ const Columns = styled(Flex)`
 `
 
 const Column = styled(Box)`
+  min-height: 600px;
   text-align: center;
   border-right: 1px solid black;
   padding: 1.5rem;
@@ -53,6 +53,28 @@ const Column = styled(Box)`
       border-bottom: none;
     }
   }
+`
+
+const RecentWork = styled(Section)`
+  padding: 1.5rem;
+
+`
+
+const Grid = styled(Box)`
+  box-sizing: border-box;
+  margin: 0px;
+  min-width: 0px;
+  display: grid;
+  gap: 20px;
+  /* grid-template-columns: repeat(auto-fit, minmax(128, 1fr)); */
+  grid-template-columns: repeat(3, 1fr);
+`
+
+const GridItem = styled(Box)`
+  border: 1px solid black;
+  border-radius: 8px;
+  padding: 90px 0;
+  text-align: center;
 `
 
 export default function Home() {
@@ -97,6 +119,16 @@ export default function Home() {
           </Column>
         </Columns>
       </Categories>
+      <RecentWork>
+        <Grid>
+          <GridItem ><h3>St. Croix Ballet</h3></GridItem>
+          <GridItem><h3>Frederic Remington</h3></GridItem>
+          <GridItem><h3>HelpMeBounce</h3></GridItem>
+          <GridItem><h3>Cohen Esray</h3></GridItem>
+          <GridItem><h3>Taylor Farms</h3></GridItem>
+        </Grid>
+      </RecentWork>
+      
     </Layout>
   )
 }
