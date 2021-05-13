44d07a0e765001ec9a2d604947f8f7d5d7704598
Barry Molina <bazzaboy@gmail.com>
2021-05-13 16:45:18 -0500

    delete old project

--- roerick-portfolio/components/Header/Header.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-import styled from 'styled-components'
-import { BaseContainer } from '../BaseContainer'
-
-export const Header = props => <BaseContainer as="header" {...props} />
--- roerick-portfolio/components/Text/Paragraph.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { BaseText } from './BaseText'
-
-const Paragraph = props => (
-  <BaseText as='p' fontSize={[2, 3]} { ...props} />
-)
-
-export { Paragraph as P }
